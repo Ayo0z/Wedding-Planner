713495608b8d704b2a365d61a52690e6183d1959
Ayo0z <happyayaali@gmail.com>
2023-12-22 16:21:14 +0100

    [TASK] First commit

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registry</title>
+    <link rel="stylesheet" href="styles.css" type="text/css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=League+Script&display=swap" rel="stylesheet">
+</head>
+<body style="background-color: #c0b6a8">
+<header id="header-registry">
+    <div class="container">
+        <div class="header-text">
+            <a href="index.html"><p>Mustafa & Aya</p></a>
+        </div>
+    </div>
+</header>
+
+<main class="login-main">
+    <div class="login-page">
+        <div class="form">
+            <form class="login-form">
+                <input type="text" placeholder="Your Name"/>
+                <input type="password" placeholder="Invitation Code"/>
+                <button class="form-btn">login</button>
+                <span class="reg-text">Or <a href="register.php">Register</a> now!</span>
+            </form>
+        </div>
+    </div>
+</main>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,38 @@
+<?php
+
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registry</title>
+    <link rel="stylesheet" href="styles.css" type="text/css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=League+Script&display=swap" rel="stylesheet">
+</head>
+<body style="background-color: #c0b6a8">
+<header id="header-registry">
+    <div class="container">
+        <div class="header-text">
+            <a href="index.html"><p>Mustafa & Aya</p></a>
+        </div>
+    </div>
+</header>
+
+<main class="register-main">
+    <div class="register-page">
+        <div class="form">
+            <form class="register-form">
+                <input type="text" placeholder="Your Name"/>
+                <input type="password" placeholder="Invitation Code"/>
+                <input type="number" placeholder="Amount of money you want to add"/>
+                <button class="form-btn">Register</button>
+                <span class="reg-text">Or <a href="login.php">Login</a> now!</span>
+            </form>
+        </div>
+    </div>
+</main>
+</body>
+</html>
